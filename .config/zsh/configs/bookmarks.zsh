7a7db905e15b936bb01bfbd7659351cce10e68cc
Rohan Ramchand <rohan@neon.xyz>
2022-08-25 14:07:56 -0700

    Make some small changes here and there

--- .config/zsh/configs/bookmarks.zsh
+++ .config/zsh/configs/bookmarks.zsh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
-hash -d att=~/src/all-the-things/
-hash -d salt=~/src/salt/
-hash -d pigeon=~/src/pigeon/
-
-hash -d sql=~/src/queries/
 hash -d tmp=~/tmp/
-
 hash -d rust=~/src/personal/rust/
+hash -d neon=~/src/neon
